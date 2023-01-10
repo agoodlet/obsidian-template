38dfe4162502abaa8b6daf1d6f3de8e4ce1c4363
Aadyn Goodlet <agoodlet@nexigen.digital>
2023-01-10 20:35:27 +1000

    adding modal

--- main.ts
+++ main.ts
@@ -1,137 +1,55 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Editor, Notice, Plugin, FileManager, Vault } from "obsidian";
+import { InsertLinkModal } from "./modal";
 
-// Remember to rename these classes and interfaces!
+// instantiate a cont for the output path for files
+// prepare a var that is the "template" to be put in the new file
+// when the user enters the branch name, it will create a file prefilled with the content
+// vault.create(path, data);
 
-interface MyPluginSettings {
-	mySetting: string;
+export default class InsertLinkPlugin extends Plugin {
+  async onload() {
+    this.addCommand({
+      id: "insert-link",
+      name: "Insert link",
+      editorCallback: (editor: Editor) => {
+        const selectedText = editor.getSelection();
+
+        const onSubmit = (url: string) => {
+			if (/^(feature|Improvement|hotfix|bugfix|master)\//g.test(url)) {
+				const regex: RegExp = /((?<=((Improvement|feature|hotfix|bugfix)\/))([a-zA-Z]{2,3})\-\d*)|((?<=\d\-).*)/g;
+				const matches = [...url.matchAll(regex)];
+				const title = matches[1][0].replace(/\-/g, ' ');
+				editor.replaceRange(`[[${matches[0][0]}]] - ${titleCase(title)}`, editor.getCursor());
+			 } else {
+				new Notice("Was not a git branch");
+			 };
+        };
+		const create = (path: string) => {
+			// const fm = new FileManager;
+			// const absolutePath = vault.getAbstractFileByPath('tasks/CCP-761');
+			editor.replaceRange(vaultName(), editor.getCursor());
+		}
+        new InsertLinkModal(this.app, onSubmit, create).open();
+      },
+    });
+
+	 // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+	 const statusBarItemEl = this.addStatusBarItem();
+	 statusBarItemEl.setText('ur mum');
+  }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+function vaultName() {
+	const vault = new Vault;
+	const name = vault.getName();
+	return "test";
+
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+function titleCase(str: string) {
+	var splitStr = str.toLowerCase().split(' ');
+	for (var i = 0; i < splitStr.length; i++) {
+		splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
 	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
+	return splitStr.join(' '); 
+ }
